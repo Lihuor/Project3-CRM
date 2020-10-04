4e451526143d3c5aa2f05b15894c8b71dc791316
Lihuor <lihuor@gmail.com>
2020-10-04 11:34:48 +1100

    Init home and App JS

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Home from './components/pages/Home';
+import InventoryList from './components/inventories/InventoryList';
+import InventoryInfo from './components/inventories/InventoryInfo';
+import InventoryAdd from './components/inventories/InventoryAdd';
+import InventoryEdit from './components/inventories/InventoryEdit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <Router>
+        <Navigation />
+        <div className="container">
+          <Main />
+        </div>
+      </Router>
     </div>
+  );
+}
+
+function Navigation() {
+  return(
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+      <div className='container'>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
+          <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/inventories">Inventories</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+function Main() {
+  return(
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/inventories" component={InventoryList} />
+      <Route exact path="/inventories/new" component={InventoryAdd} />
+      <Route exact path="/inventories/:_id" component={InventoryInfo} />
+      <Route exact path="/inventories/:_id/edit" component={InventoryEdit} />
+    </Switch>
   );
 }
 
