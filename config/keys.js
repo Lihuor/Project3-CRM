d41006e15c787ac8e11d49926f2b611eb406d6a3
Lihuor <lihuor@gmail.com>
2020-10-06 00:01:30 +1100

    Init Keyjs

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://dbUser:lihuor123@cluster0.gnrp9.mongodb.net/crm-users?retryWrites=true&w=majority'
+};
--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const mongoose = require('mongoose');
 // const cors = require('cors');
 const router = require('./routes/index');
 
-const PORT = 5000;
-const MONGODB_URI = 'mongodb+srv://dbUser:lihuor123@cluster0.gnrp9.mongodb.net/crm-users?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+// DB Config
+const db = require('./config/keys').MONGODB_URI;
 
 // app.use(cors())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use('/api', router); 
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false }); 
 mongoose.connection.once('open', function() { 
   console.log('Connected to the Database.');
 });
