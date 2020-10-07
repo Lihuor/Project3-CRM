d22e0f09c8a20f41823f0d17776d4be44409b585
Lihuor <lihuor@gmail.com>
2020-10-07 20:03:57 +1100

    Init login register backend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB_URI: 'mongodb+srv://dbUser:lihuor123@cluster0.gnrp9.mongodb.net/crm-users?retryWrites=true&w=majority'
+    MONGODB_URI: 'mongodb+srv://dbUser:lihuor123@cluster0.gnrp9.mongodb.net/crm-users?retryWrites=true&w=majority',
+    secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 // const cors = require('cors');
 const router = require('./routes/index');
+const passport = require("passport");
 
 const PORT = process.env.PORT || 5000;
 
+// Bodyparser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // DB Config
 const db = require('./config/keys').MONGODB_URI;
+const users = require("./routes/api/users");
 
 // app.use(cors())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use('/api', router); 
 
+// Connecting to MongoDB
 mongoose.connect(db, { useNewUrlParser: true, useFindAndModify: false }); 
 mongoose.connection.once('open', function() { 
   console.log('Connected to the Database.');
@@ -21,5 +29,12 @@ mongoose.connection.once('open', function() {
 mongoose.connection.on('error', function(error) {
   console.log('Mongoose Connection Error : ' + error);
 });
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 app.listen(PORT, function() { console.log(`Server listening on port ${PORT}`) });
