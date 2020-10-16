45601fd767075e828622a919644a3d33066df324
Lihuor <lihuor@gmail.com>
2020-10-17 09:17:43 +1100

    Added Image

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,6 +4,8 @@ function Home() {
   return (
     <div className="jumbotron">
       <h1>Welcome to My ABC Company Pty.ltd</h1>
+      <img src="https://github.com/Lihuor/Project3-CRM-V2/blob/main/client/public/Images/Warehousepic1.jpg" alt="Warehouse1" width="500" height="600"></img>
+      <img src="https://github.com/Lihuor/Project3-CRM-V2/blob/main/client/public/Images/Warehousepic2.jpg" alt="Warehouse1" width="500" height="600"></img>
     </div>
   );
 }
